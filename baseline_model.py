d37b875e280f34925b4c57edd0884b748d4d2eec
Ziqian Qiu <ziqian.qiu@mail.utoronto.ca>
2020-11-17 18:32:06 -0500

    add cases where multiple possible diseases found

--- baseline_model.py
+++ baseline_model.py
@@ -1,6 +1,3 @@
-import csv
-import json
-# d= {(10,"M"): {"d1":5, "d2":10}, (11,"M"): {"d1":3, "d2":2}}
 def helper_merge_two_dicts(d1, d2):
     d = d1
     for keys in d2:
@@ -32,9 +29,15 @@ with open("Data_Entry_2017.csv", mode="r") as file:
 
 def helper_find_key(d: dict):
     max_value = max(d.values())
+    res = ""
     for i in d:
         if d[i] == max_value:
-            return i
+            if res != "":
+                res += " or "
+            res += i
+
+    return res
+
 
 def get_baseline_model(age, gender):
     if not isinstance(age, int) or gender not in ["M", "F"] or age not in range(0, 155):
@@ -45,5 +48,7 @@ def get_baseline_model(age, gender):
         gender_string = "male"
     print("a", age, "years old", gender_string, "'s most likely medical condition is:", helper_find_key(bm_dict[input_key]))
 
-get_baseline_model(30,"F")
-get_baseline_model(91,"M")
+
+get_baseline_model(90, "F")
+get_baseline_model(80, "F")
+get_baseline_model(91, "M")
