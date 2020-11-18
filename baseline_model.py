e04b713f35f367edd4d684cd9b2dd3b550412d04
Ziqian Qiu <ziqian.qiu@mail.utoronto.ca>
2020-11-17 20:03:43 -0500

    baseline model accuracy added

--- baseline_model.py
+++ baseline_model.py
@@ -35,20 +35,45 @@ def helper_find_key(d: dict):
             if res != "":
                 res += " or "
             res += i
-
     return res
 
 
 def get_baseline_model(age, gender):
     if not isinstance(age, int) or gender not in ["M", "F"] or age not in range(0, 155):
-        print("the input is incorrect")
+        pass
+        # print(age, gender)
+        # print("the input is incorrect")
     input_key = str(str(age) + gender)
     gender_string = "female"
     if gender == "M":
         gender_string = "male"
-    print("a", age, "years old", gender_string, "'s most likely medical condition is:", helper_find_key(bm_dict[input_key]))
+    # print("a", age, "years old", gender_string, "'s most likely medical condition is:", helper_find_key(bm_dict[input_key]))
+    return helper_find_key(bm_dict[input_key])
 
 
-get_baseline_model(90, "F")
-get_baseline_model(80, "F")
+get_baseline_model(63, "F")
+get_baseline_model(88, "M")
+get_baseline_model(80, "M")
 get_baseline_model(91, "M")
+
+
+def get_baseline_model_accuracy():
+    correct = 0
+    wrong = 0
+    with open("Data_Entry_2017.csv", mode="r") as file:
+        for i in file.readlines()[1:]:
+            age = i.split(",")[4]
+            gender = i.split(",")[5]
+            disease_list = i.split(",")[1]
+            predicted_disease = get_baseline_model(int(age), gender)
+            if "Cardiomegaly" not in disease_list and "No Finding" not in disease_list and "Effusion" not in disease_list\
+                and "Infiltration" not in disease_list and "Mass" not in disease_list:
+                disease_list = "No Finding"
+            if predicted_disease in disease_list:
+                correct += 1
+            else:
+                wrong += 1
+    print("the accuracy is", correct/(correct+ wrong) * 100, "%")
+
+
+get_baseline_model_accuracy()
